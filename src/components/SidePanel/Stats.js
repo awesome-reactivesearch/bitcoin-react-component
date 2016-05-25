7ad6aa38394262f84243906e8ad72a08080f5f89
Yash Shah <mail@yashshah.com>
2016-05-25 19:35:26 +0530

    Refactor code

--- src/components/SidePanel/Stats.js
+++ src/components/SidePanel/Stats.js
@@ -25,7 +25,6 @@ var Stats = React.createClass({
   },
   componentDidMount: function(){
     var self = this;
-
     appbaseRef.searchStream(requestObject).on('data', function(stream) {
       self.setState({bid: stream._source.bid});
       self.setState({last: stream._source.last});
@@ -35,23 +34,16 @@ var Stats = React.createClass({
     }).on('error', function(error) {
       console.log('Error handling code');
     });
-    return {
-      bid: "0",
-      last: "0",
-      avg: "0",
-      total: "0",
-      ask: "0"
-    };
   },
-  render : function(){
-
+  render: function(){
     return (
       <div>
         <div className="bid-center">
           <p className="label-text">BID</p>
           </div>
           <center>
-            <div id="odometer" className="odometer odometer-theme-digital">{this.state.bid}
+            <div id="odometer" className="odometer odometer-theme-digital">
+              {this.state.bid}
             </div>
           </center>
           <div className="col-md-6">
