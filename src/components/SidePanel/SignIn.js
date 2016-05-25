eb50ae8371dc70b8ce2d44a22f60137c7a4d4fe3
Yash Shah <mail@yashshah.com>
2016-05-25 18:27:38 +0530

    Fix the code

--- src/components/SidePanel/SignIn.js
+++ src/components/SidePanel/SignIn.js
@@ -1,108 +1,84 @@
 var React = require('react');
 var stat = require('./Stats.js');
-var Appbase = require("appbase-js");
-var config = {
-  "appname": "jsfiddle-demo",
-  "username": "7eJWHfD4P",
-  "password": "431d9cea-5219-4dfb-b798-f897f3a02665",
-  "type": "1463991450935"
-};
-var appbaseRef = new Appbase({
-  url: 'https://scalr.api.appbase.io',
-  appname: config.appname,
-  username: config.username,
-  password: config.password
-});
+var appbaseRef = require("../appbase").appbaseRef;
 
-
-var SignIpForm = React.createClass({
+var AlertForm = React.createClass({
   getInitialState: function(){
     return {
       price: '0',
-      opt: 'lessthan',
-      greaterprice: '0',
-      lessprice: '0'
+      email: '',
+      selectValue: ''
     }
   },
-  handleClick: function(){
-    console.log(this.refs.input1.getDOMNode().value)
-    var self = this;
-    var lte;
-    var gte;
-    console.log(self.state.opt)
-    if (self.state.opt == 'lessthan') {
-      gte = 0
-      lte =this.refs.input1.getDOMNode().value
-      self.setState({greaterprice: 0});
-      self.setState({lessprice: self.refs.input1});
+  setWebhook: function(){
+    var lteParam;
+    var gteParam;
+    var price = this.state.price
+    var selectValue = this.state.selectValue
+    var email = this.state.email
+    if (selectValue == 'lte') {
+      gteParam = 0
+      lteParam = price
     }
-    else if (self.state.opt == 'greaterthan') {
-      lte = 1000
-      gte =this.refs.input1.getDOMNode().value
-      self.setState({greaterprice: self.refs.input1});
-      self.setState({lessprice: 1000});
+    else if (selectValue == 'gte') {
+      lteParam = 1000
+      gteParam =price
     }
     else {
-      gte = this.refs.input1.getDOMNode().value
-      lte =this.refs.input1.getDOMNode().value
-      self.setState({greaterprice: self.refs.input1});
-      self.setState({lessprice: self.refs.input1})
+      gteParam = price
+      lteParam = price
     }
-    var temp = {
-      "query":{
-        "range" : {
-          "last" : {
-            "gte" : gte,
-            "lte" : lte
-          }
-        }
-      }
-    }
-    console.log("",temp);
-      appbaseRef.searchStreamToURL({
-        type: config.type,
-        body:{
-          "query":{
-            "range" : {
-              "last" : {
-                "gte" : parseFloat(gte),
-                "lte" : parseFloat(lte)
-              }
+    appbaseRef.searchStreamToURL({
+      type: config.type,
+      body:{
+        "query":{
+          "range" : {
+            "last" : {
+              "gte" : parseFloat(gteParam),
+              "lte" : parseFloat(lteParam)
             }
           }
         }
-      },{
-        'method': 'POST',
-        'url': 'http://requestb.in/1874f2q1',
-        "count":1
-      }).on('data', function(response) {
-          console.log("Webhook has been configured : ", response);
-      }).on('error', function(error) {
-          console.log("searchStreamToURL() failed with: ", error)
-      });
+      }
+    },{
+      'method': 'POST',
+      'url': 'http://requestb.in/1874f2q1',
+      "count": 1
+    }).on('data', function(response) {
+        console.log("Webhook has been configured : ", response);
+    }).on('error', function(error) {
+        console.log("searchStreamToURL() failed with: ", error)
+    });
   },
-  handleChange: function(){
-    console.log(this.refs.opt1.getDOMNode().value)
+  handlePriceChange: function(event){
     this.setState({
-      price: event.target.input,
-      opt: this.refs.opt1.getDOMNode().value
+      price: event.target.value,
+    });
+  },
+  handleEmailChange: function(event){
+    this.setState({
+      email: event.target.value,
+    });
+  },
+  handleSelectChange: function(event){
+    this.setState({
+      selectValue: event.target.value
     });
   },
   render : function() {
     return (
       <div className="row max" id="main">
-      <br/>
-      <select id="select_type" ref="opt1" className="form-control small" value={this.state.opt} onChange={this.handleChange}>
-      <option value="lessthan">Lesser Than</option>
-      <option value="greaterthan">Greater Than</option>
-      <option value="fixvalue">Fix Value</option>
-      </select>
-      <input type="text" className="form-control small" ref="input1" placeholder="Enter Value" id="upperprice" value={this.state.price} onChange={this.handleChange} required/><br/>
-      <input type="email" className="form-control big" placeholder="E-mail" name="email" required/><br/>
-      <input type="submit" value="Submit" id="submit" className="btn btn-primary" onClick={this.handleClick}/><br/>
-
+        <br/>
+        <select ref="conditionSelect" className="form-control small" value={this.state.selectValue} onChange={this.handleSelectChange}>
+          <option value="lte">Lesser Than</option>
+          <option value="gte">Greater Than</option>
+          <option value="fixvalue">Fix Value</option>
+        </select>
+        <input type="text" className="form-control small" placeholder="Enter price" value={this.state.price} onChange={this.handlePriceChange} required/><br/>
+        <input type="email" className="form-control big" placeholder="Enter email address" value={this.state.email} onChange={this.handleEmailChange} required/><br/>
+        <input type="submit" className="btn btn-primary" onClick={this.setWebhook}/><br/>
       </div>
     )
   }
 });
-module.exports = SignIpForm;
+module.exports = AlertForm;
