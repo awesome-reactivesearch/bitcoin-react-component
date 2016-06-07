422e3d8f8359a2400521be55d21f980affde1291
Yash Shah <mail@yashshah.com>
2016-06-07 12:16:20 +0530

    Fix rendering of AlertForm

--- src/components/AlertForm.js
+++ src/components/AlertForm.js
@@ -60,15 +60,23 @@ var AlertForm = React.createClass({
   render : function() {
     return (
       <div className="row max" id="main">
-        <br/>
-        <select ref="conditionSelect" className="form-control small" value={this.state.selectValue} onChange={this.handleSelectChange}>
-          <option value="lte">Lesser Than</option>
-          <option value="gte">Greater Than</option>
-          <option value="fixvalue">Fix Value</option>
-        </select>
-        <input type="text" className="form-control small" placeholder="Enter price" value={this.state.price} onChange={this.handlePriceChange} required/><br/>
-        <input type="email" className="form-control big" placeholder="Enter email address" value={this.state.email} onChange={this.handleEmailChange} required/><br/>
-        <input type="submit" className="btn btn-primary" onClick={this.setWebhook}/><br/>
+      <br />
+        <div className="col-md-6">
+          <select ref="conditionSelect" className="form-control" value={this.state.selectValue} onChange={this.handleSelectChange}>
+            <option value="lte">Lesser Than</option>
+            <option value="gte">Greater Than</option>
+            <option value="fixvalue">Fix Value</option>
+          </select>
+        </div>
+        <div className="col-md-6">
+          <input type="text" className="form-control" placeholder="Enter price" value={this.state.price} onChange={this.handlePriceChange} required/><br/>
+        </div>
+        <div className="col-md-12">
+          <input type="email" className="form-control" placeholder="Enter email address" value={this.state.email} onChange={this.handleEmailChange} required/><br/>
+        </div>
+        <div className="col-md-6 col-md-offset-3">
+          <input type="submit" className="btn btn-primary" onClick={this.setWebhook}/><br/>
+        </div>
       </div>
     )
   }
